f6d7ce7a3afc9250818d02a26b24a7a2fa2f411b
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-16 20:59:07 -0400

    Creates route and method for goodbye and number

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -3,4 +3,14 @@ class GreetingsController < ApplicationController
     input_value = params["wildcard"]
     render json: { message: "#{input_value}" }
   end
+
+  def goodbye
+    input_value = params["message"]
+    render json: { message: "#{input_value}" }
+  end
+
+  def number
+    input_value = params["blue_potato"]
+    render json: { message: "#{input_value}" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "home/:wildcard" => "greetings#home"
-  get "home/:wildcard" => "greetings#goodbye"
-  get "home/:wildcard" => "greetings#123"
+  get "home/:wildcard" => "greetings#greeting"
+  get "home/:message" => "greetings#goodbye"
+  get "home/:blue_potato" => "greetings#number"
 end
